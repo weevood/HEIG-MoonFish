81428acafbfece923f6008cad09ee3eb5310254f
weevood <5528482+weevood@users.noreply.github.com>
2021-07-15 17:08:35 +0200

    Angular tests

--- 4.Tests-Technos/Angular/src/app/app.component.ts
+++ 4.Tests-Technos/Angular/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
-})
+import {AccountService} from './_services';
+import {User} from './_models';
+
+@Component({selector: 'app', templateUrl: 'app.component.html'})
 export class AppComponent {
-  title = 'Angular';
+  user!: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
--- 4.Tests-Technos/Angular/src/app/app.module.ts
+++ 4.Tests-Technos/Angular/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ErrorInterceptor} from './_helpers';
+import {AppComponent} from './app.component';
+import {AlertComponent} from './_components';
+import {HomeComponent} from './home';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent
+  ],
+
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+  ],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+};
