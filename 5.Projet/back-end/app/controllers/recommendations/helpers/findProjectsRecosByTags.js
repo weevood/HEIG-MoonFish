c8fca21fafbb41554ce722390e88835d733606e3
weevood <5528482+weevood@users.noreply.github.com>
2021-09-10 16:06:26 +0200

    Documentation Recommendations

--- 5.Projet/back-end/app/controllers/recommendations/helpers/findProjectsRecosByTags.js
+++ 5.Projet/back-end/app/controllers/recommendations/helpers/findProjectsRecosByTags.js
@@ -3,26 +3,40 @@ const { clearNode } = require('../../../middleware/utils')
 const { setProjectInfo, findProject } = require('../../projects/helpers')
 const { STATUS_ACTIVE } = require('../../../models/enums/status')
 const { PROJECT_STATUS_ENDED, PROJECT_STATUS_PROPOSAL } = require('../../../models/enums/projectStatus')
+const { REF_PROJECTS_LIMIT } = require('../../../../config/constants')
 
 /**
- * Find user by ID
- * @param {uuid} uuid - the node uuid
+ * Find recommended projects by tags similarities
+ *
+ * @param {uuid} uuid - the main project uuid
  * @param {int} limit - the maximum number of recommendations to retrieve
  */
 const findProjectsRecosByTags = (uuid, limit = 10) => {
     return new Promise(async (resolve, reject) => {
-        const REF_PROJECTS_LIMIT = 3
+        // const query = `MATCH (u:User {uuid: '${uuid}'})-[rmo:IS_MEMBER_OF {status: ${STATUS_ACTIVE}}]
+        //                     ->(t:Team)-[rd:DEVELOPS]->(p:Project)<-[rm:MANDATES]-(m:Team)
+        //                 WHERE p.status = ${PROJECT_STATUS_ENDED}
+        //                 WITH p
+        //                 ORDER BY rm.mark DESC
+        //                 LIMIT ${REF_PROJECTS_LIMIT}
+        //                 WITH COLLECT(apoc.convert.fromJsonList(p.tags)) AS tags
+        //                 WITH REDUCE(output = [], t IN tags | output + t) AS flatTags
+        //                 MATCH (recs:Project)
+        //                 WHERE recs.status = ${PROJECT_STATUS_PROPOSAL}
+        //                     AND any(t IN apoc.convert.fromJsonList(recs.tags) WHERE t IN flatTags)
+        //                 RETURN recs AS recommendations, flatTags
+        //                 LIMIT ${limit}`
         const query = `MATCH (u:User {uuid: '${uuid}'})-[rmo:IS_MEMBER_OF {status: ${STATUS_ACTIVE}}]
                             ->(t:Team)-[rd:DEVELOPS]->(p:Project)<-[rm:MANDATES]-(m:Team)
                         WHERE p.status = ${PROJECT_STATUS_ENDED}
                         WITH p
                         ORDER BY rm.mark DESC
                         LIMIT ${REF_PROJECTS_LIMIT}
-                        WITH COLLECT(apoc.convert.fromJsonList(p.tags)) AS tags
+                        WITH COLLECT(p.tags) AS tags
                         WITH REDUCE(output = [], t IN tags | output + t) AS flatTags
                         MATCH (recs:Project)
-                        WHERE recs.status = ${PROJECT_STATUS_PROPOSAL} 
-                            AND any(t IN apoc.convert.fromJsonList(recs.tags) WHERE t IN flatTags)
+                        WHERE recs.status = ${PROJECT_STATUS_PROPOSAL}
+                            AND any(t IN recs.tags WHERE t IN flatTags)
                         RETURN recs AS recommendations, flatTags
                         LIMIT ${limit}`
         await neo4j.cypher(query, {})
